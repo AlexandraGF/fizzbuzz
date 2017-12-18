3bbc2e18159fb3f1d46c11770e39ee68f54563c6
Alexy <alexandra.fina@yahoo.com>
2017-12-18 20:12:05 +0000

    Refactored production code

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,13 @@
+class FizzBuzz
+  def divisible_by(number)
+    if number % 15 == 0
+      "FizzBuzz"
+    elsif number % 3 == 0
+      "Fizz"
+    elsif number % 5 == 0
+      "Buzz"
+    else
+      number
+    end
+  end
+end
